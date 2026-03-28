1b578eba73266bad0ed8ed779e9aadc5f032f570
Lucas Martinez <lucasgmartinez7@gmail.com>
2026-03-28 11:58:56 -0300

    clases

---
+++ Python/classes/day-02/05-mathematical-operators.py
@@ -0,0 +1,19 @@
+#Operadores matemáticos
+
+x = 6
+y = 2
+z = 7
+
+print(f"{x} más {y} es igual a {x+y}")
+print(f"{x} menos {y} es igual a {x-y}")
+print(f"{x} por {y} es igual a {x*y}")
+print(f"{x} dividido {y} es igual a {x/y}")
+
+print(f"{z} dividido al piso por {y} es igual a {z//y}") #La operación redondea para abajo
+print(f"{z} módulo {y} es igual a {z%y}") #El resultado corresponde a lo que "sobra" de la operación
+
+print(f"{x} elevado a la {y} es igual a {x**y}")
+print(f"{x} elevado a la 3 es igual a {x**3}")
+
+print(f"La raíz cuadrada de {x} es igual a {x**0.5}")
+
