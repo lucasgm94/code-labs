589da3995eea2e1f6fd7d4b22be99c7e016b2ebc
Lucas Martinez <lucasgmartinez7@gmail.com>
2026-03-28 12:06:09 -0300

    Clean up: Removed IDE and virtual env folders

--- Python/Actividades/cerveceria.py
+++
@@ -1,4 +0,0 @@
-print("El nombre de tu cervecería\nes "
-      + input("¿Qué ciudad te gustaría visitar?: ")
-      + " "
-      + input("¿Cuál es tu color favorito?: ") + "\nFelicitaciones!!!!")
--- Python/Proyectos/mini-project1.py
+++
@@ -1,5 +0,0 @@
-print("Tu cerveza se llama:\n"
-      +"\""
-      +(input("¿En donde naciste?: "))
-      + " "
-      + (input("¿Cómo se llamaba tu primer mascota?: ")+"\""))
